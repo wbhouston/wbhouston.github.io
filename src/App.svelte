54181e7be73bb65daf4368f8dbcd0d8186f03a22
William Houston <wmbhouston@gmail.com>
2022-06-28 22:48:05 -0400

    Add sign-up and access-token

--- src/App.svelte
+++ src/App.svelte
@@ -1,63 +1,23 @@
 <script>
-  import Counter from './lib/Counter.svelte'
+  let match = (window.location.hash) ?
+    window.location.hash.match(/#access_token=([^&]+)/) :
+    false;
+
+  let access_token;
+
+  if(match[1]) {
+    access_token = match[1];
+  } else {
+    access_token = false
+  }
 </script>
 
 <main>
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  {#if access_token}
+    <p> Signed in! {access_token}</p>
+  {:else}
+    <a href="https://www.arcgis.com/sharing/rest/oauth2/authorize?client_id=MaDm9SQi6MlFzZfr&response_type=token&expiration=20160&redirect_uri=https://wbhouston.github.io/" >
+      Sign In to ArcGIS
+    </a>
+  {/if}
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
