d98420cb21d4b7a47ee241dffd8d9c8c0a14002a
William Houston <wmbhouston@gmail.com>
2022-06-28 21:49:57 -0400

    Initial commit

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/wbhouston/wbhouston.github.io.git',
+        user: {
+            name: 'William Houston',
+            email: 'wmbhouston@gmail.com'
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
